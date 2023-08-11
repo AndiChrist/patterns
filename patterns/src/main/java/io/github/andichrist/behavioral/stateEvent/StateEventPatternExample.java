dfad88dfabb1b3a9ce288e18f0cc635edb84dd1e
Andreas Christ <andreas.christ@allane.com>
2023-08-11 14:04:57 +0200

    removed wrong source

--- patterns/src/main/java/io/github/andichrist/behavioral/stateEvent/StateEventPatternExample.java
+++ patterns/src/main/java/io/github/andichrist/behavioral/stateEvent/StateEventPatternExample.java
@@ -1,15 +1,6 @@
 package io.github.andichrist.behavioral.stateEvent;
 
 /*
- Das "State/Event" Pattern ist ein Entwurfsmuster aus dem Buch "Java Design Patterns" von
- William C. Wake und Paul K. Phillips. Es beschreibt eine Methode zur Implementierung von
- Zustandsautomaten in Java-Anwendungen, indem sowohl Zustände als auch Ereignisse
- berücksichtigt werden.
-
- Stuart Sierra hat das Buch "Java Design Patterns" mitverfasst, aber das "State/Event"
- Pattern ist nicht direkt mit ihm verbunden. Es ist vielmehr eine Darstellung eines
- Zustandsautomatenmusters, das in Java-Anwendungen verwendet werden kann.
-
  Das "State/Event" Pattern verwendet Zustandsübergänge und Ereignisse, um das Verhalten
  eines Objekts in verschiedenen Zuständen zu modellieren. Hier sind die Hauptkomponenten
  des Musters:
